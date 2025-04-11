278c7542a8371fbae0e443f39801bd383422e20c
Atharva-techonsy <atharva.chaudhari@techonsy.com>
2025-04-11 16:01:11 +0530

    CICD Test 2

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR CICD is Working properly
+          Edit <code>src/App.jsx</code> and save to test HMR CICD is  properly
         </p>
       </div>
       <p className="read-the-docs">
